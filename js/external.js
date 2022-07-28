e17fe60b14f7001dae86718c36318b4f58b55cdf
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-07-28 14:12:30 -0500

    update files

--- js/external.js
+++ js/external.js
@@ -1,4 +1,5 @@
 // JavaScript with HTML
+
 console.log("Hello from external JavaScript");
 
 alert("Welcome to my website!");
@@ -9,3 +10,21 @@ if(userInput === "blue") {
 } else {
     alert("Awesome color choice!");
 }
+
+// Movie rentals
+
+// const perDayRate = 3;
+// let littleMermaidDays = Number(prompt("How many days for Little Mermaid?"));
+// let brotherBearDays = Number(prompt("How many days for Brother Bear?"));
+// let hercules = Number(prompt("How many days for Hercules?"));
+// let total = littleMermaidDays + brotherBearDays + hercules;
+// alert(`Your total is $${total}`);
+
+// Contractor pay
+
+const googleRate = 400;
+const facebookRate = 350;
+const amazonRate = 380;
+let googleHrs = prompt("How many hours worked for Google?");
+let googleHrs = prompt("How many hours worked for Google?");
+let googleHrs = prompt("How many hours worked for Google?");
