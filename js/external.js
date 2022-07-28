2e26f6db730c989e50918ab66b29dbb0b314071a
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-07-28 16:23:17 -0500

    refactor external.js

--- js/external.js
+++ js/external.js
@@ -1,40 +1,42 @@
-// JavaScript with HTML
+// JavaScript with HTML //
 
-// console.log("Hello from external JavaScript");
-//
-// alert("Welcome to my website!");
-// let userInput = prompt("What is your favorite color?").toLocaleLowerCase();
-// if(userInput === "blue") {
-//     alert("Cool, that's my favorite color too!");
-// } else {
-//     alert("Awesome color choice!");
-// }
+console.log("Hello from external JavaScript");
 
-// Movie rentals
+alert("Welcome to my website!");
+let userInput = prompt("What is your favorite color?").toLocaleLowerCase();
+if(userInput === "blue") {
+    alert("Cool, that's my favorite color too!");
+} else {
+    alert("Awesome color choice!");
+}
 
-// const perDayRate = 3;
-// let littleMermaidDays = Number(prompt("How many days for Little Mermaid?"));
-// let brotherBearDays = Number(prompt("How many days for Brother Bear?"));
-// let herculesDays = Number(prompt("How many days for Hercules?"));
-// let total = (littleMermaidDays * perDayRate) +
-//             (brotherBearDays * perDayRate) +
-//             (herculesDays * perDayRate);
-// alert(`Your total is $${total.toFixed(2)}`);
+// Movie rentals //
 
-// Contractor pay
+const perDayRate = 3;
+let littleMermaidDays = Number(prompt("How many days for Little Mermaid?"));
+let brotherBearDays = Number(prompt("How many days for Brother Bear?"));
+let herculesDays = Number(prompt("How many days for Hercules?"));
+let total = (littleMermaidDays * perDayRate) +
+            (brotherBearDays * perDayRate) +
+            (herculesDays * perDayRate);
 
-// const googleRate = 400;
-// const facebookRate = 350;
-// const amazonRate = 380;
-// let googleHrs = prompt("How many hours worked for Google?");
-// let facebookHrs = prompt("How many hours worked for Facebook?");
-// let amazonHrs = prompt("How many hours worked for Amazon?");
-// let totalPay =  googleRate * Number(googleHrs) +
-//                 facebookRate * Number(facebookHrs) +
-//                 amazonRate * Number(amazonHrs);
-// alert(`Your total pay is $${totalPay.toFixed(2)}`);
+alert(`Your total is $${total.toFixed(2)}`);
 
-// College registration
+// Contractor pay //
+
+const googleRate = 400;
+const facebookRate = 350;
+const amazonRate = 380;
+let googleHrs = prompt("How many hours worked for Google?");
+let facebookHrs = prompt("How many hours worked for Facebook?");
+let amazonHrs = prompt("How many hours worked for Amazon?");
+let totalPay =  googleRate * Number(googleHrs) +
+                facebookRate * Number(facebookHrs) +
+                amazonRate * Number(amazonHrs);
+
+alert(`Your total pay is $${totalPay.toFixed(2)}`);
+
+// College registration //
 
 const maxStudents = 15;
 let isConflict = false;
@@ -56,19 +58,19 @@ if(!isConflict && randomStudents <= maxStudents) {
     alert("Sorry can't enroll, conflict in your schedule.");
 }
 
-// Product Offer
+// Product Offer //
 
 let memberItems = 1;
 let premiumMember = true;
 let offerExpired = false;
 
-// Generate offers and expired offers
+//Generate offers and expired offers
 const randomOffer = Math.random();
 if (randomOffer <= .5) {
     offerExpired = true;
 }
-
 alert("Proceed to checkout");
+
 // Conditional to check if discount applied
 if(!offerExpired && memberItems > 2 || !offerExpired && premiumMember) {
     alert("Congratulations! Discount applied!");
