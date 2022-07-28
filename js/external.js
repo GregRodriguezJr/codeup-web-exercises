5cad37bb4769f0d2f6e689f391481c0398842adf
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-07-28 12:46:58 -0500

    added movie rental

--- js/external.js
+++ js/external.js
@@ -1,12 +1,11 @@
+// JavaScript with HTML
 console.log("Hello from external JavaScript");
 
 alert("Welcome to my website!");
 let userInput = prompt("What is your favorite color?").toLocaleLowerCase();
 
-if (userInput === "blue") {
+if(userInput === "blue") {
     alert("Cool, that's my favorite color too!");
 } else {
     alert("Awesome color choice!");
 }
-
-
---
+++ js/js_html_exercise_3.js
@@ -0,0 +1,7 @@
+// Movie rentals
+const perDayRate = 3;
+let littleMermaidDays = prompt("How many days for Little Mermaid?");
+let brotherBearDays = prompt("How many days for Brother Bear?");
+let hercules = prompt("How many days for Hercules?");
+let total = littleMermaidDays + brotherBearDays + hercules;
+alert(`Your total is $${total}`);
