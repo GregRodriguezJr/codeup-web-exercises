e3c88747ddce21b3b3e97d00c24bac2a32b26bbe
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-07-28 14:29:38 -0500

    added contractor pay

--- js/external.js
+++ js/external.js
@@ -4,7 +4,6 @@ console.log("Hello from external JavaScript");
 
 alert("Welcome to my website!");
 let userInput = prompt("What is your favorite color?").toLocaleLowerCase();
-
 if(userInput === "blue") {
     alert("Cool, that's my favorite color too!");
 } else {
@@ -13,12 +12,12 @@ if(userInput === "blue") {
 
 // Movie rentals
 
-// const perDayRate = 3;
-// let littleMermaidDays = Number(prompt("How many days for Little Mermaid?"));
-// let brotherBearDays = Number(prompt("How many days for Brother Bear?"));
-// let hercules = Number(prompt("How many days for Hercules?"));
-// let total = littleMermaidDays + brotherBearDays + hercules;
-// alert(`Your total is $${total}`);
+const perDayRate = 3;
+let littleMermaidDays = Number(prompt("How many days for Little Mermaid?"));
+let brotherBearDays = Number(prompt("How many days for Brother Bear?"));
+let hercules = Number(prompt("How many days for Hercules?"));
+let total = littleMermaidDays + brotherBearDays + hercules;
+alert(`Your total is $${total}`);
 
 // Contractor pay
 
@@ -26,5 +25,9 @@ const googleRate = 400;
 const facebookRate = 350;
 const amazonRate = 380;
 let googleHrs = prompt("How many hours worked for Google?");
-let googleHrs = prompt("How many hours worked for Google?");
-let googleHrs = prompt("How many hours worked for Google?");
+let facebookHrs = prompt("How many hours worked for Facebook?");
+let amazonHrs = prompt("How many hours worked for Amazon?");
+let totalPay =  googleRate * Number(googleHrs) +
+                facebookRate * Number(facebookHrs) +
+                amazonRate * Number(amazonHrs);
+alert(`Your total pay is $${totalPay.toFixed(2)}`);
