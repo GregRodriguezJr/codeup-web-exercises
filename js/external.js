6d4d6a35e2ae9db069595071bbb2ecfd2d0cf306
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-07-28 09:33:37 -0500

    added conditional

--- js/external.js
+++ js/external.js
@@ -1,1 +1,12 @@
-console.log("Hello from external JavaScript")
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my website!");
+let userInput = prompt("What is your favorite color?").toLocaleLowerCase();
+
+if (userInput === "blue") {
+    alert("Cool, that's my favorite color too!");
+} else {
+    alert("Awesome color choice!");
+}
+
+
