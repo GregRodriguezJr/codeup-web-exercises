ef160e1382af15dcaa68a039a8226ed5b6e00f7d
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-08-03 17:21:24 -0500

    completed intro to arrays

--- js/iterating.js
+++ js/iterating.js
@@ -7,11 +7,15 @@
      * 'names'.
      */
 
+    let names = ["Jane", "Joe", "Vince", "Will"];
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+
+    console.log(names.length);
 
     /**
      * TODO:
@@ -19,16 +23,29 @@
      * accessing each element's index.
      */
 
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+    
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
 
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+
+    names.forEach(name => {
+        console.log(name);
+    });
 
     /**
      * TODO:
@@ -44,4 +61,17 @@
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
 
+    function first(array) {
+        return array[0];
+    }
+    function second(array) {
+        return array[1];
+    }
+    function last(array) {
+        return array[4];
+    }
+
+    console.log(first([1, 2, 3, 4, 5])); // returns 1
+    console.log(second([1, 2, 3, 4, 5])); // returns 2
+    console.log(last([1, 2, 3, 4, 5])) // return 5);
 })();
