3113586ae7d6e42174237c0b6d9ed7c3d0d5b0e1
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-08-04 11:04:44 -0500

    added objects files

--- js/planets-string.js
+++ js/planets-string.js
@@ -14,18 +14,21 @@
     planetsArray = planetsString.split('|');
     console.log(typeof planetsArray, planetsArray);
 
-    /**
-     * TODO:
-     * Create a string with <br> tags between each planet. console.log() your
-     * results. Why might this be useful?
-     *
-     * BONUS:
-     * Create another string that would display your planets in an undordered
-     * list. You will need an opening AND closing <ul> tags around the entire
-     * string, and <li> tags around each planet.
-     */
-
+    
+    /*     
+    * TODO:
+    * Create a string with <br> tags between each planet. console.log() your
+    * results. Why might this be useful? 
+    */
+     
     console.log(planetsArray.join(' <br> '));
+
+    /*      
+    * BONUS:
+    * Create another string that would display your planets in an undordered
+    * list. You will need an opening AND closing <ul> tags around the entire
+    * string, and <li> tags around each planet. 
+    */
     
     let newArray = [];
     planetsArray.forEach(planet => {
