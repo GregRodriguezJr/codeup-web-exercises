fd3d2b584de45f25b4779b6d45722b9fe07c5acd
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-08-08 14:32:30 -0500

    completed break n continue js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,10 +3,15 @@
 // Use a loop and the continue statement to output all the odd numbers between 1 and 50, 
 // except for the number the user entered.
 
-// do {
-//     console.log("test");
-//     let userNum = 5;
-//     console.log(userNum);
-// } while (isNaN(userNum) || userNum % 2 === 0 ||
-//     userNum >= 50 || userNum < 1);
 
+
+do {
+    userNum = prompt("Please enter a number between 1 and 50");
+    for (let i = 1; i <= 50; i++) {
+        if(i % 2 !== 0 && i === Number(userNum)) {
+            console.log(`Skipping number: ${i}`);
+        } else if (i % 2 !== 0) {
+            console.log(`Here is an odd number: ${i}`);
+        }
+    }
+} while (isNaN(userNum) || userNum % 2 === 0 || userNum >= 50 || userNum < 1);
