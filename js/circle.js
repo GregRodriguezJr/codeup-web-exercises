670f2fdcdce45e244c1fb109553bf73d85770a2c
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-08-08 16:02:04 -0500

    completed math exercises

--- js/circle.js
+++ js/circle.js
@@ -8,17 +8,21 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            return Math.PI * Math.pow(this.radius, 2);
+            // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
+            let areaResult;
+            if (doRounding) {
+                areaResult = Math.round(this.getArea());  
+            } else {
+                areaResult = this.getArea();
+            }
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log(`Area of a circle with radius: ${this.radius}, is: ${areaResult}`);
         }
     };
 
@@ -31,9 +35,11 @@
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
 
+    circle.radius = 5;
+
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
+    circle.logInfo(true); 
 })();
