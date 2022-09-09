0b5a0c0fcd98b7f4d3d059cddaea7c8df73a2d7d
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-09 11:38:51 -0500

    added navbar and bg image

--- js/jQuery/ajax-blog.js
+++ js/jQuery/ajax-blog.js
@@ -3,17 +3,18 @@ const localUrl = "../../data/blog.json"
 $.get(localUrl)
     .done((data) => {
         const blogPosts = data;
-        blogPosts.forEach(blogPost => {
+        // Create card for each blog post
+        blogPosts.forEach((blogPost,index) => {
             $('#post').append(`
-                <div class="card">
-                    <div class="card-header">
+                <div class="card my-4">
+                    <div class="card-header bg-light">
                         <h3>${blogPost.title}</h3>
                     </div>
                     <div class="card-body">
                         <p>Categories: ${blogPost.categories}</p>
                         <p>${blogPost.date}</p>
                         <p>${blogPost.content}</p>
-                        <button class="btn btn-primary">Photos</button>
+                        <button id="blogPost${index}" class="btn btn-primary">Photos</button>
                     </div>
                 </div>
             `);
