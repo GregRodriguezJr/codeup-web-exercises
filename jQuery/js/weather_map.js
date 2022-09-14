6e30a96986fa768a20472a68a256e3b56a5d888c
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-14 09:52:31 -0500

    added geocode function, search input event listener

--- jQuery/js/weather_map.js
+++ jQuery/js/weather_map.js
@@ -11,15 +11,40 @@ var map = new mapboxgl.Map({
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
-// Event listener to capture user input
-$('.mapboxgl-ctrl-geocoder--input').click(() => {
-    let searchInput = $('.mapboxgl-ctrl-geocoder--input').val();
-    console.log(searchInput);
-})
+// Method to search for coordinates based on a physical address and return
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+        // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
 
-let lon = -96.8033;
+// Event listener to capture user input
+$('#search-input-btn').click(() => {
+    const searchInput = $('#search-input').val();
+    geocode(searchInput, mapboxgl.accessToken).then((result) => {
+        // Set new map from users search
+        map.setCenter(result);
+        map.setZoom(14);
+        // New marker on the map from user input
+        let marker = new mapboxgl.Marker({draggable: true})
+        .setLngLat([result[0], result[1]])
+        .addTo(map);
+        let coordinates = marker.getLngLat();   
+        console.log(coordinates);
+        lng = coordinates.lng;
+        lat = coordinates.lat;
+    });
+});
+
+let lng = -96.8033;
 let lat = 32.7778;
-const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${WEATHER_API_TOKEN}`
+const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&units=imperial&appid=${WEATHER_API_TOKEN}`
 
 // Render forecast to the DOM with widgets
 const renderWidgets = (cityId) => {
