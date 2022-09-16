e5ae9218c4f1b032c1d2f419de8631d231b94382
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-16 17:33:36 -0500

    completed todo 1, 2

--- javaScript_ll/map-filter-reduce.js
+++ javaScript_ll/map-filter-reduce.js
@@ -36,13 +36,14 @@ const users = [
     }
 ];
 
-// 1. Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+// 1. Create a file named map-filter-reduce.js in your js directory and copy the users data below into it.
+// 2. Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 
 const newArray = users.filter((user) => user.languages.length >= 3);
 console.log(newArray);
 
-// 2. Use .map to create an array of strings where each element is a user's email address
-// 3. Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
-// 4. Use .reduce to get the longest email from the list of users.
-// 5. Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+// 3. Use .map to create an array of strings where each element is a user's email address
+// 4. Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+// 5. Use .reduce to get the longest email from the list of users.
+// 6. Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 // Bonus Use .reduce to get the unique list of languages from the list of users.
