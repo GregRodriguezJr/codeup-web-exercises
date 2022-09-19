a6cf30a83de6afc058a63214c0d82e26406ada44
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-19 12:56:57 -0500

    added format date logic, new renderHtml function

--- javaScript_ll/js/promises.js
+++ javaScript_ll/js/promises.js
@@ -1,8 +1,25 @@
-let username = "GregRodriguezJr"
+let username = "shivasamadhi"
 
-fetch( `https://api.github.com/users/${username}/events/public`, 
+const renderHtml = (dataObj, date) => {
+    const { type,payload: {commits: [{message}]}, repo: {name : repoName} } = dataObj;
+    console.log(type, message, repoName);
+    console.log(date);
+}
+
+// API call to fetch github data
+const fetchGithub = (username) => {
+    fetch( `https://api.github.com/users/${username}/events/public`, 
     { headers: {'Authorization': GITHUB_API_TOKEN}})
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-    })
+        const dataObj = data[0]
+        const date = dataObj.created_at;
+        console.log(dataObj);
+        // Format date to day/month/year
+        const formatedDate = `${date.substring(5,7)}/${date.substring(8,10)}/${date.substring(0,4)}`;
+        // Call function to render data to the DOM
+        renderHtml(dataObj, formatedDate);
+    });
+}
+fetchGithub(username);
+
