356c561d9fdac69fa6ed3dac9fb8aeeab14c4bd9
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-19 14:05:47 -0500

    refactored api call with async/await function

--- javaScript_ll/js/promises.js
+++ javaScript_ll/js/promises.js
@@ -1,25 +1,38 @@
 let username = "shivasamadhi"
 
+// API call to fetch github data
+// const fetchGithub = (username) => {
+//     fetch( `https://api.github.com/users/${username}/events/public`, 
+//     { headers: {'Authorization': GITHUB_API_TOKEN}})
+//     .then(res => res.json())
+//     .then(data => {
+//         const dataObj = data[0]
+//         const date = dataObj.created_at;
+//         // Format date to day/month/year
+//         const formatedDate = `${date.substring(5,7)}/${date.substring(8,10)}/${date.substring(0,4)}`;
+//         // Call function to render data to the DOM
+//         renderHtml(dataObj, formatedDate);
+//     });
+// }
+// fetchGithub(username);
+
 const renderHtml = (dataObj, date) => {
     const { type,payload: {commits: [{message}]}, repo: {name : repoName} } = dataObj;
-    console.log(type, message, repoName);
-    console.log(date);
+    console.log(type, message, repoName, date);
 }
 
 // API call to fetch github data
-const fetchGithub = (username) => {
-    fetch( `https://api.github.com/users/${username}/events/public`, 
-    { headers: {'Authorization': GITHUB_API_TOKEN}})
-    .then(res => res.json())
-    .then(data => {
-        const dataObj = data[0]
-        const date = dataObj.created_at;
-        console.log(dataObj);
+async function fetchGithub(username){
+    let url = `https://api.github.com/users/${username}/events/public`
+    try {
+        const response = await fetch(url, { headers: {'Authorization': GITHUB_API_TOKEN} });
+        const data = await response.json();
+        const date = data[0].created_at;
         // Format date to day/month/year
         const formatedDate = `${date.substring(5,7)}/${date.substring(8,10)}/${date.substring(0,4)}`;
-        // Call function to render data to the DOM
-        renderHtml(dataObj, formatedDate);
-    });
+        renderHtml(data[0], formatedDate);
+    } catch (error) {
+        console.log(error);
+    }
 }
 fetchGithub(username);
-
